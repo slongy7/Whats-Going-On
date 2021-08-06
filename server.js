b58eb2671eb94c9f20761ec82d7b2149250cb777
slongy7 <79759725+slongy7@users.noreply.github.com>
2021-08-05 20:50:56 -0400

    updated

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+const path = require('path');
 const express = require('express');
-//const routes = require('./controllers/homeRoutes');
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
 const app = express();
+const hbs = exphbs.create({});
+
 
 
 const PORT = process.env.PORT || 3001;
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 
 
 app.use(express.urlencoded({ extended: true }));
-//app.use(routes);
+app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/homeRoutes'));
 
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
