61990a835bc924c4ab4b80fd9c8e5bca3251b7c5
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-08-10 08:39:05 -0400

    Created and init business model

---
+++ models/business.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+ 
+class Business extends Model { }
+ 
+Business.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING(100),
+        },
+        city: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        zip: {
+            type: DataTypes.INTEGER(100),
+            allowNull: false,
+        },
+        type: {
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        tableName: 'BUSINESS',
+        underscored: true,
+        modelName: 'business',
+    }
+)
+ 
+module.exports = Business;
