e4bb9e8143d7c7f230642699ac1f690eb94e392b
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-08-10 08:38:22 -0400

    Created and init user model

--- models/user.js
+++ models/user.js
@@ -2,3 +2,52 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+class User extends Model { }
+ 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        first_name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        pw: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+            validate: {
+                len: [30],
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        tableName: 'USER',
+        underscored: true,
+        modelName: 'user',
+    }
+)
+ 
+module.exports = User;
