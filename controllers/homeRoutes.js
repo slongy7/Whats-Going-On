a2be009ee47dc25bdf3bdf48ce87e37be0dab912
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-08-14 09:51:57 -0400

    location update for api fcns

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Business } = require('../models/index');
-const { findRestList } = require('../apiPull');
+const { findRestList } = require('../utils/apiPull');
 
 
 router.get('/', async (req, res) => {
