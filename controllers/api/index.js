742edd7a3cf4a5c65c510451106c1a690032e30e
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-08-14 21:41:45 -0400

    fix user signup error

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const businessRoutes = require('./businessRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/', businessRoutes);
+router.use('/business', businessRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
