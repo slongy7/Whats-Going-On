c144886aff251265393893a334eeed95e2791a5e
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-08-15 23:06:14 -0400

    create functionality to have business data appear on screen

--- controllers/api/businessRoutes.js
+++ controllers/api/businessRoutes.js
@@ -2,8 +2,20 @@ const router = require('express').Router();
 const Business = require('../../models/business');
 const {findRestList} = require('../homeRoutes');
 
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
     // findRestList('columbus', 'bar', res);
+    Business.findAll({
+        attributes: [
+            'id',
+            'name',
+            'address',
+            'category'
+        ]
+    }).then(dbBusinessData => res.json(dbBusinessData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
     console.log('POST HERE', req.body);
 });
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,21 @@ const { findRestList } = require('../utils/apiPull');
 
 router.get('/', async (req, res) => {
     findRestList('columbus', 'bar');
-    res.render('homepage');
+    Business.findAll({
+        attributes: [
+            'id',
+            'name',
+            'address',
+            'category'
+        ]
+    }).then(dbBusinessData => {
+        const businesses = dbBusinessData.map(business => business.get({plain: true}));
+        res.render('homepage', {businesses});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
     
 });
 
